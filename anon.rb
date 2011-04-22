eac2573fdbbb5463e6dbe1c9b606bf26342907a5
Mike Lewis <ft.mikelewis@gmail.com>
2011-04-22 17:03:16 -0400

    Better randomization for non duplicates

--- anon.rb
+++ anon.rb
@@ -1,5 +1,18 @@
 require 'csv'
 
+#core extension
+class Array
+  def destructive_sample_except(except_num)
+    rand_val = rand(self.size)
+    until self[rand_val] != except_num do
+      rand_val = rand(self.size)
+    end
+
+    delete_at(rand_val)
+  end
+
+end
+
 class Anon
   attr_reader :secure_attrs, :columns, :buckets
   def initialize(file_name)
@@ -14,7 +27,14 @@ class Anon
           csved = line.parse_csv
           unless line.start_with?('@') || line.chomp.empty?
             @secure_attrs.each do |secure_attr|
-              until(rand_val = @buckets[csved[-2].to_sym][secure_attr].sample) != csved[secure_attr] do end
+              bucket = csved[-2].to_sym
+              rand_val = 
+                if @buckets[bucket][secure_attr].size == 1
+                  @buckets[bucket][secure_attr].first[1]
+                else
+                  #until(val = @buckets[bucket][secure_attr].destructive_sample) != csved[secure_attr] do end
+                  @buckets[bucket][secure_attr].destructive_sample_except([data_file.lineno, csved[secure_attr]])[1]
+                end
               csved[secure_attr] = rand_val
             end
           end
@@ -42,9 +62,18 @@ class Anon
           #csv lines
           csved = line.parse_csv
           @secure_attrs.each do |secure_attr|
-            ((@buckets[csved[-2].to_sym] ||= {})[secure_attr] ||= []) << csved[secure_attr]
+            ((@buckets[csved[-2].to_sym] ||= {})[secure_attr] ||= []) << [data_file.lineno, csved[secure_attr]]
           end
         end
+      end
+    end
+    check_validity
+  end
+
+  def check_validity
+    buckets.each do |k,v|
+      if v.first[1].size == 1
+        raise "Need more than one instance per class"
       end
     end
   end
@@ -53,8 +82,11 @@ end
 
 if __FILE__ == $0
   abort "#{$0} file_name" unless ARGV.size == 1
+  puts "Preproccessing"
   anonymize = Anon.new(ARGV[0])
+  puts "Processing"
   anonymize.process
+  puts "Done"
 end
 
 
